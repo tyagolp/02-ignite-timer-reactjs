f620fbb5c3e347bd7318a94f2334109b6c8fd61a
Tiago <tyagolp@gmail.com>
2022-07-20 11:26:32 -0300

    add eslint in the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { ThemeProvider } from 'styled-components';
-import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
 
-import { GlobalStyle } from './styles/global';
-import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>Test</h1>
